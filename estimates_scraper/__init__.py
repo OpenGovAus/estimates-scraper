e7846f9008b97547a25e574ff879a26c91a639b2
king-millez <Millez.Dev@gmail.com>
2021-04-12 15:20:21 +1000

    get portfolios

--- estimates_scraper/__init__.py
+++ estimates_scraper/__init__.py
@@ -5,11 +5,21 @@ from requests.api import head
 URL = 'https://www.aph.gov.au/Parliamentary_Business/Senate_estimates/Next_hearings'
 
 def scrape_committees():
-    _headers = BeautifulSoup(get(URL).text, 'lxml').find('div', {'_rdeditor_temp': '1'}).find_all('h3')
-    _dates = []
+    _soup = BeautifulSoup(get(URL).text, 'lxml')
+    YEAR = _soup.find('span', {'class': 'not-bold'}).find('br').nextSibling.strip()[-4:]
+    _headers = _soup.find('div', {'_rdeditor_temp': '1'}).find_all('h3')
+    hearing_list = []
     for header in _headers:
+        portfolios = []
+        for comm in header.findNext('div').find_all('div', {'class': 'box'}):
+            for comm_name in comm.find('ul').find_all('li'):
+                portfolios.append(comm_name.text)
+            print(portfolios)
+        
         try:
-            _dates.append(header.find('br').nextSibling.strip().replace('\xa0', ' '))
+            _date = header.find('br').nextSibling.strip().replace('\xa0', ' ')
         except:
-            _dates.append(header.text.replace('\xa0', ' '))
-    print(_dates)
+            _date = header.text.replace('\xa0', ' ')
+        hearing_list.append({'date': _date, 'portfolios': portfolios})
+    return hearing_list
+        
